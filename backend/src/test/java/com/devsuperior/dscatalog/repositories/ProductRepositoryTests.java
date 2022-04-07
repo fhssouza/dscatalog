83f6810daf5d82b997470af946eb8a134987ae8a
fhssouza <fhssouza@gmail.com>
2022-04-07 17:32:06 -0300

    Test Save Id Null

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,21 +10,36 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long exintinId;
 	private long nonExistingId;
-	
-	
+	private long countTotalProducts;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		exintinId = 1L;
 		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+
+	@Test
+	public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+
+		Product product = Factory.createProduct();
+		product.setId(null);
+
+		product = repository.save(product);
+
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+
 	}
 
 	@Test
@@ -36,11 +51,11 @@ public class ProductRepositoryTests {
 		Assertions.assertFalse(result.isPresent());
 
 	}
-	
+
 	@Test
 	public void deleteShoulThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-		
-		Assertions.assertThrows(EmptyResultDataAccessException.class, ()-> {
+
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
 		});
 	}
